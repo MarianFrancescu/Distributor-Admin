f5fe0dd256fe77bee33ec7525194ba11d511454e
Marian Francescu <marian.francescu98@gmail.com>
2022-03-29 14:20:14 +0300

    Change the face of login component

--- src/components/login/login.tsx
+++ src/components/login/login.tsx
@@ -1,4 +1,6 @@
-import React, {useState} from "react";
+import { Box, TextField, FormControlLabel, Checkbox, Button, Grid, Avatar, Typography } from "@mui/material";
+import React, { useState } from "react";
+import LockOpenRoundedIcon from '@mui/icons-material/LockOpenRounded';
 import './login.css'
 
 function Login() {
@@ -11,26 +13,48 @@ function Login() {
         console.log('user', email, password);
     }
 
-    const handleFieldChange = (event: React.ChangeEvent<HTMLInputElement>) => {
-        setEmail(event.target.value);
-        setPassword(event.target.value);
-    }
-
-    return(
-        <div>
-            <form onSubmit={e => { handleSubmit(e) }} className="login-container">
-                <label>
-                    Email
-                    <input type="text" name="email" onChange={handleFieldChange} placeholder="email"/>
-                </label>
-                <label>
-                    Password
-                    <input type="password" name="password" onChange={handleFieldChange} placeholder="password"/>
-                </label>
-                <div>
-                    <input type="submit" value="Submit" />
-                </div>
-            </form>
+    return (
+        <div className="login-card">
+            <div className="login-container">
+                <Avatar sx={{ m: 1, bgcolor: 'primary.main' }}>
+                    {<LockOpenRoundedIcon />}
+                </Avatar>
+                <Typography component="h1" variant="h5">
+                    Sign in
+                </Typography>
+                <Box component="form" onSubmit={handleSubmit} className="login-box" noValidate sx={{ mt: 1 }}>
+                    <TextField
+                        margin="normal"
+                        required
+                        fullWidth
+                        id="email"
+                        label="Email Address"
+                        name="email"
+                        autoComplete="email"
+                        autoFocus
+                        onChange={(e) => setEmail(e.target.value)}
+                    />
+                    <TextField
+                        margin="normal"
+                        required
+                        fullWidth
+                        name="password"
+                        label="Password"
+                        type="password"
+                        id="password"
+                        autoComplete="current-password"
+                        onChange={(e) => setPassword(e.target.value)}
+                    />
+                    <Button
+                        type="submit"
+                        fullWidth
+                        variant="contained"
+                        sx={{ mt: 3, mb: 2 }}
+                    >
+                        Sign In
+                    </Button>
+                </Box>
+            </div>
         </div>
     );
 }
