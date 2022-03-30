7f15b0978784b697ce7034cb2b5498982223f23f
Marian Francescu <marian.francescu98@gmail.com>
2022-03-30 10:53:02 +0300

    Add some interfaces

---
+++ src/models/discipline.interface.ts
@@ -0,0 +1,17 @@
+export interface Discipline {
+    _id?: string;
+    teacher: string;
+    name: string;
+    timetable?: Timetable[];
+    studyInstitution: string;
+    faculty: string;
+    department: string;
+    studyYear: string;
+    maxNoOfStudentsPerTimetable: number;
+    created?: string;
+  }
+  
+  interface Timetable {
+    option: string;
+    students?: string[];
+  }
---
+++ src/models/user.interface.ts
@@ -0,0 +1,14 @@
+export interface User {
+    _id: string;
+    email: string;
+    firstName: string;
+    lastName: string;
+    password: string;
+    registrationNumber: string;
+    studyInstitution: string;
+    faculty: string;
+    department: string;
+    studyYear: string;
+    created: Date;
+    role: string;
+  }
