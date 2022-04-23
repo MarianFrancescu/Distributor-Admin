540154baa89c57f6fef78daf0ad8f91f7dacfaad
Marian Francescu <marian.francescu98@gmail.com>
2022-04-23 21:11:51 +0300

    Restyled some components for mobile view

--- src/components/discipline-delete-dialog/discipline-delete-dialog.tsx
+++ src/components/discipline-delete-dialog/discipline-delete-dialog.tsx
@@ -1,32 +1,31 @@
 import { Button, Modal, Backdrop, Fade, Box, Typography } from "@mui/material";
 import React from "react";
-import './discipline-delete-dialog.scss';
+import "./discipline-delete-dialog.scss";
 import service from "../../services/service";
 
 const style = {
-    position: 'absolute' as 'absolute',
-    top: '50%',
-    left: '50%',
-    transform: 'translate(-50%, -50%)',
-    width: 400,
-    bgcolor: 'background.paper',
-    border: '2px solid #000',
-    boxShadow: 24,
-    p: 4,
+  position: "absolute" as "absolute",
+  top: "50%",
+  left: "50%",
+  transform: "translate(-50%, -50%)",
+  width: 400,
+  bgcolor: "background.paper",
+  border: "2px solid #000",
+  boxShadow: 24,
+  p: 4,
+};
+
+function DisciplineDeleteDialog({ openModal, closeModal, disciplineID }: any) {
+  // const [open, setOpen] = React.useState(openModal);
+  // const handleOpen = () => setOpen(true);
+  // const handleClose = () => setOpen(false);
+  const handleDelete = () => {
+    service.deleteDiscipline(disciplineID);
+    closeModal();
   };
 
-function DisciplineDeleteDialog ({openModal, closeModal, disciplineID}: any) {
-
-    // const [open, setOpen] = React.useState(openModal);
-    // const handleOpen = () => setOpen(true);
-    // const handleClose = () => setOpen(false);
-    const handleDelete = () => {
-      service.deleteDiscipline(disciplineID);
-      closeModal();
-    }
-
-    return (
-        <div>
+  return (
+    <div>
       {/* <Button onClick={handleOpen}>Open modal</Button> */}
       <Modal
         aria-labelledby="transition-modal-title"
@@ -47,13 +46,15 @@ function DisciplineDeleteDialog ({openModal, closeModal, disciplineID}: any) {
             <Typography id="transition-modal-description" sx={{ mt: 2 }}>
               Delete this discipline?
             </Typography>
-            <Button onClick={closeModal}>Close</Button>
-            <Button onClick={handleDelete}>Delete</Button>
+            <div className="actions-area">
+              <Button onClick={closeModal}>Close</Button>
+              <Button variant="contained" color="error" onClick={handleDelete}>Delete</Button>
+            </div>
           </Box>
         </Fade>
       </Modal>
     </div>
-    )
+  );
 }
 
 export default DisciplineDeleteDialog;
--- src/components/student-delete-dialog/student-delete-dialog.tsx
+++ src/components/student-delete-dialog/student-delete-dialog.tsx
@@ -3,52 +3,54 @@ import React from "react";
 import service from "../../services/service";
 
 const style = {
-    position: 'absolute' as 'absolute',
-    top: '50%',
-    left: '50%',
-    transform: 'translate(-50%, -50%)',
-    width: 400,
-    bgcolor: 'background.paper',
-    border: '2px solid #000',
-    boxShadow: 24,
-    p: 4,
+  position: "absolute" as "absolute",
+  top: "50%",
+  left: "50%",
+  transform: "translate(-50%, -50%)",
+  width: 400,
+  bgcolor: "background.paper",
+  border: "2px solid #000",
+  boxShadow: 24,
+  p: 4,
+};
+
+function StudentDeleteDialog({ openModal, closeModal, userID }: any) {
+  const handleDelete = () => {
+    service.deleteUser(userID);
+    closeModal();
   };
-  
-function StudentDeleteDialog({openModal, closeModal, userID}: any) {
-    const handleDelete = () => {
-        service.deleteUser(userID);
-        closeModal();
-      }
-  
-      return (
-          <div>
-        {/* <Button onClick={handleOpen}>Open modal</Button> */}
-        <Modal
-          aria-labelledby="transition-modal-title"
-          aria-describedby="transition-modal-description"
-          open={openModal}
-          onClose={closeModal}
-          closeAfterTransition
-          BackdropComponent={Backdrop}
-          BackdropProps={{
-            timeout: 500,
-          }}
-        >
-          <Fade in={openModal}>
-            <Box sx={style}>
-              <Typography id="transition-modal-title" variant="h6" component="h2">
-                Warning!
-              </Typography>
-              <Typography id="transition-modal-description" sx={{ mt: 2 }}>
-                Delete this student?
-              </Typography>
+
+  return (
+    <div>
+      {/* <Button onClick={handleOpen}>Open modal</Button> */}
+      <Modal
+        aria-labelledby="transition-modal-title"
+        aria-describedby="transition-modal-description"
+        open={openModal}
+        onClose={closeModal}
+        closeAfterTransition
+        BackdropComponent={Backdrop}
+        BackdropProps={{
+          timeout: 500,
+        }}
+      >
+        <Fade in={openModal}>
+          <Box sx={style}>
+            <Typography id="transition-modal-title" variant="h6" component="h2">
+              Warning!
+            </Typography>
+            <Typography id="transition-modal-description" sx={{ mt: 2 }}>
+              Delete this student?
+            </Typography>
+            <div className="actions-area">
               <Button onClick={closeModal}>Close</Button>
-              <Button onClick={handleDelete}>Delete</Button>
-            </Box>
-          </Fade>
-        </Modal>
-      </div>
-      )
+              <Button variant="contained" color="error" onClick={handleDelete}>Delete</Button>
+            </div>
+          </Box>
+        </Fade>
+      </Modal>
+    </div>
+  );
 }
 
 export default StudentDeleteDialog;
